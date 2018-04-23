6ccac0dc20b1b784f0af125b5c0f5e01cfc12d55
ilya radu <raduilya@gmail.com>
2018-04-24 02:45:17 +0300

    fix problem with account icon in dashboard

--- app/components/ApBarDashboard/ApBarDashboard.js
+++ app/components/ApBarDashboard/ApBarDashboard.js
@@ -3,28 +3,25 @@ import {AppBar, Button, IconButton, Toolbar, Typography} from 'material-ui';
 import AccountCircle from 'material-ui-icons/AccountCircle';
 import Menu, {MenuItem} from 'material-ui/Menu';
 import './ApBarDashboard.scss';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Dashboard from "../Dashboard/Dashboard";
-import History from "../History/History";
-import Leaderboard from "../Leaderboard/Leaderboard";
-import MarketPlace from "../MarketPlace/MarketPlace";
-import NewsTournaments from "../NewsTournaments/NewsTournaments";
-
-
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import History from '../History/History';
+import Leaderboard from '../Leaderboard/Leaderboard';
+import MarketPlace from '../MarketPlace/MarketPlace';
+import NewsTournaments from '../NewsTournaments/NewsTournaments';
 
 
 export default class ApBarDashboard extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            match : props.match
-        };
-    }
-  state = {
-    auth: true,
-    anchorEl: null,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      match: props.match,
+      auth: true,
+      anchorEl: null,
+    };
+  }
+
 
   handleChange = (event, checked) => {
     this.setState({auth: checked});
@@ -43,63 +40,66 @@ export default class ApBarDashboard extends Component {
     const open = Boolean(anchorEl);
 
     return (
-        <Router>
+      <Router>
         <div>
-        <div key="background" className="appBarDashboardBG">
-        </div>
-        <div key="main" className="appBarDashboardMain">
-          <AppBar position="static" style={{background: 'none'}}>
-            <Toolbar>
-              <Typography variant="title">
-                <div className="logo"></div>
-              </Typography>
-                <Link to={`${this.state.match.url}/dashboard`}><Button className="buttonAppBar">Dashboard</Button></Link>
+          <div key="background" className="appBarDashboardBG">
+          </div>
+          <div key="main" className="appBarDashboardMain">
+            <AppBar position="static" style={{background: 'none'}}>
+              <Toolbar>
+                <Typography variant="title">
+                  <div className="logo"></div>
+                </Typography>
+                <Link to={`${this.state.match.url}/dashboard`}><Button
+                  className="buttonAppBar">Dashboard</Button></Link>
                 <Link to={`${this.state.match.url}/history`}><Button className="buttonAppBar">History</Button></Link>
-                <Link to={`${this.state.match.url}/leaderboard`}><Button className="buttonAppBar">Leaderboared</Button></Link>
-                <Link to={`${this.state.match.url}/marketPlace`}><Button className="buttonAppBar">Market place</Button></Link>
-                <Link to={`${this.state.match.url}/newsTournaments`}><Button className="buttonAppBar">News and Tournaments</Button></Link>
-              {auth && (
-                <div>
-                  <IconButton
-                    aria-owns={open ? 'menu-appbar' : null}
-                    aria-haspopup="true"
-                    onClick={this.handleMenu}
-                    color="inherit"
-                  >
-                    <AccountCircle/>
-                  </IconButton>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={this.handleClose}
-                  >
-                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                  </Menu>
-                </div>
-              )}
-            </Toolbar>
-          </AppBar>
+                <Link to={`${this.state.match.url}/leaderboard`}><Button
+                  className="buttonAppBar">Leaderboared</Button></Link>
+                <Link to={`${this.state.match.url}/marketPlace`}><Button className="buttonAppBar">Market
+                  place</Button></Link>
+                <Link to={`${this.state.match.url}/newsTournaments`}><Button className="buttonAppBar">News and
+                  Tournaments</Button></Link>
+                {auth && (
+                  <div>
+                    <IconButton
+                      aria-owns={open ? 'menu-appbar' : null}
+                      aria-haspopup="true"
+                      onClick={this.handleMenu}
+                      color="inherit"
+                    >
+                      <AccountCircle/>
+                    </IconButton>
+                    <Menu
+                      id="menu-appbar"
+                      anchorEl={anchorEl}
+                      anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      open={open}
+                      onClose={this.handleClose}
+                    >
+                      <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                      <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                    </Menu>
+                  </div>
+                )}
+              </Toolbar>
+            </AppBar>
+            {console.log('this.state.match.url.',this.state.match.url)  }
+            <Route path={`${this.state.match.url}/dashboard`} component={Dashboard}/>
+            <Route path={`${this.state.match.url}/history`} component={History}/>
+            <Route path={`${this.state.match.url}/leaderboard`} component={Leaderboard}/>
+            <Route path={`${this.state.match.url}/marketPlace`} component={MarketPlace}/>
+            <Route path={`${this.state.match.url}/newsTournaments`} component={NewsTournaments}/>
 
-            <Route path={`${this.state.match.url}/dashboard`} component={Dashboard} />
-            <Route path={`${this.state.match.url}/history`} component={History} />
-            <Route path={`${this.state.match.url}/leaderboard`} component={Leaderboard} />
-            <Route path={`${this.state.match.url}/marketPlace`} component={MarketPlace} />
-            <Route path={`${this.state.match.url}/newsTournaments`} component={NewsTournaments} />
-
+          </div>
         </div>
-        </div>
-
-        </Router>
+      </Router>
     );
   }
 }
--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 
 import LoginPage from './containers/LoginPage';
-import WalletPage from './containers/WalletPage';
 import LoggedInPage from './containers/LoggedInPage';
 import RegistrationPage from './containers/RegistrationPage';
-import Dashboard from "./components/Dashboard/Dashboard";
-import History from "./components/History/History";
 
 
 export default (
@@ -14,8 +11,5 @@ export default (
     <Route exact path="/" component={LoginPage} />
     <Route exact path="/loggedin" component={LoggedInPage} />
     <Route exact path="/Registration" component={RegistrationPage} />
-      {/*// TODO implement private route after authenticating*/}
-      {/*// like this https://reacttraining.com/react-router/web/example/auth-workflow*/}
-      <Route exact path="/wallet" component={WalletPage} />
   </Switch>
 );
