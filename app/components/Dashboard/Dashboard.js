cf7878a1699655b4038c3f24dae38a3cec66cbac
ilya radu <raduilya@gmail.com>
2018-04-24 03:52:09 +0300

    add papers in dashboard page

--- app/components/Dashboard/Dashboard.js
+++ app/components/Dashboard/Dashboard.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import './Dashboard.scss'
-const title = "start planing and earn coins!"
+import React, {Component} from 'react';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import './Dashboard.scss';
+
+const title = 'start planing and earn coins!';
 
 export default class Dashboard extends Component {
   render() {
@@ -13,7 +16,58 @@ export default class Dashboard extends Component {
           You will earn more coins by marking achievement in active game
         </div>
         <div className="dashboardMain">
+          <div className="papersMain">
+            <Paper className="myAcc" elevation={8}>
+              <div className="titleMyAcc">
+                My Account
+              </div>
+              <div className="balanceMyAcc">
+                <div style={{color: '#919191'}}>
+                  Buff Balance
+                </div>
+                <div style={{padding: '0 10px', color: '#347139'}}>
+                  20 coins
+                </div>
+              </div>
 
+              <div style={{borderTop: '1px solid #131416'}}/>
+
+              <div className="balanceMyAcc">
+                <div style={{color: '#919191'}}>
+                  Gold Status
+                </div>
+                <div style={{padding: '0 10px', color: '#347139'}}>
+                  New Zealand Clan
+                </div>
+              </div>
+            </Paper>
+
+          </div>
+          <div className="papersMain">
+            <Paper className="myAcc" elevation={8}>
+              <div className="titleMyAcc">
+                Active Games
+              </div>
+              <div className="balanceMyAcc">
+                <div style={{color: '#919191'}}>
+                  CS:GO
+                </div>
+
+              </div>
+              <div className="balanceMyAcc">
+                <div style={{color: '#919191'}}>
+                  Dota2
+                </div>
+              </div>
+              <div className="balanceMyAcc">
+                <div style={{color: '#919191'}}>
+                  PUBG
+                </div>
+              </div>
+
+            </Paper>
+
+          </div>
         </div>
       </div>
     );
