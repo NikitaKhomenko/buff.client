9f8bb887b6c6df67a59ca50b50eb6cc5103e246c
ilya radu <raduilya@gmail.com>
2018-04-28 15:23:12 +0300

    Fix Ui in History page

--- app/components/History/History.js
+++ app/components/History/History.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Paper from 'material-ui/Paper';
+import {Grid} from "material-ui";
 import Table, {TableBody, TableCell, TableHead, TableRow} from 'material-ui/Table';
 import './History.scss';
 
 const title = 'start planing and earn coins!';
 let id = 0;
+
 function createData(name, calories, fat, carbs, protein) {
   id += 1;
   return {id, name, calories, fat, carbs, protein};
@@ -21,45 +23,49 @@ export default class History extends Component {
   render() {
     return (
       <div className="HistoryComponent">
-        <div className="HistoryTitle">
+        <div className="HistoryTitle" style={{width: 300, height: 100}}>
           {title.toLocaleUpperCase()}
         </div>
-        <div className="contentTitle">
-          You're game and earning history:
+        <div className="contentTitle" style={{width: 550, height: 80}}>
+          You will earn more coins by marking achievement in active game
         </div>
         <div className="HistoryMain">
-          <div className="papersMain">
-            <Paper className="myAcc" elevation={8}>
-              <div className="titleHistory">
-                History
-              </div>
-              <Table className="tableHistory">
-                <TableHead>
-                  <TableRow >
-                    <TableCell className="tableColumnHead" >Date&Time</TableCell>
-                    <TableCell className="tableColumnHead">Game\Conversion</TableCell>
-                    <TableCell className="tableColumnHead">Achivements</TableCell>
-                    <TableCell className="tableColumnHead">Buff Coins</TableCell>
-                    <TableCell className="tableColumnHead">Conversion</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map(n => {
-                    return (
-                      <TableRow key={n.id}>
-                        <TableCell className="tableColumn">{n.name}</TableCell>
-                        <TableCell className="tableColumn">{n.calories}</TableCell>
-                        <TableCell className="tableColumn">{n.fat}</TableCell>
-                        <TableCell className="tableColumn">{n.carbs}</TableCell>
-                        <TableCell className="tableColumn">{n.protein}</TableCell>
+          <Grid container spacing={24}>
+            <Grid item xs={12} container>
+              <div className="papersMain">
+                <Paper className="myAcc" elevation={8}>
+                  <div className="titleHistory">
+                    History
+                  </div>
+                  <Table className="tableHistory">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell className="tableColumnHead">Date&Time</TableCell>
+                        <TableCell className="tableColumnHead">Game\Conversion</TableCell>
+                        <TableCell className="tableColumnHead">Achivements</TableCell>
+                        <TableCell className="tableColumnHead">Buff Coins</TableCell>
+                        <TableCell className="tableColumnHead">Conversion</TableCell>
                       </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </Paper>
+                    </TableHead>
+                    <TableBody>
+                      {data.map(n => {
+                        return (
+                          <TableRow key={n.id}>
+                            <TableCell className="tableColumn">{n.name}</TableCell>
+                            <TableCell className="tableColumn">{n.calories}</TableCell>
+                            <TableCell className="tableColumn">{n.fat}</TableCell>
+                            <TableCell className="tableColumn">{n.carbs}</TableCell>
+                            <TableCell className="tableColumn">{n.protein}</TableCell>
+                          </TableRow>
+                        );
+                      })}
+                    </TableBody>
+                  </Table>
+                </Paper>
 
-          </div>
+              </div>
+            </Grid>
+          </Grid>
         </div>
       </div>
     );
